b98ab94075e054b8b19d07e4a4fcfb36e90e6fe7
unknown <claderamirfrancesc@gmail.com>
2025-03-25 18:36:14 +0100

    1.0

---
+++ Datos.py
@@ -0,0 +1,28 @@
+import csv
+import re
+
+values = [['paquete', 'presión', 'temperatura']]
+
+#n presió: nnnnnnn temperatura: nn.nn\n
+def ReadData():
+    with open('data.txt', 'r', encoding="utf-8") as dataFile:
+        return dataFile.read()
+
+def Convert(txt1, txt2):
+    for i in data.split('\n'):
+        values.append(i.split(' '))
+
+    for i in values:
+        if txt1 in i: i.remove(txt1)
+        if txt2 in i: i.remove(txt2)
+
+def SaveData():
+    with open("datos.csv", mode="w", newline="", encoding="utf-8") as file:
+        escritor = csv.writer(file) 
+        escritor.writerows(values)
+    
+data = re.sub('\.', ',', ReadData())
+
+Convert('presió:', 'temperatura:')
+
+print('Archivo CSV creado con exito')
